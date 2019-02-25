7b611ca44f72b9e55b2beb806ee40e22f03af6c7
James Lea-Baran <james.c.lea@gmail.com>
2019-02-26 00:27:25 +0100

    Added spawner params

--- Source/VRTest/Spawner.cpp
+++ Source/VRTest/Spawner.cpp
@@ -1,25 +1,95 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Spawner.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "AIController.h"
+#include "Kismet/GameplayStatics.h"
 #include "ExtendedCharacter.h"
 
 // Sets default values
 ASpawner::ASpawner()
 {
+	NumSpawned = 0;
+	MaxSpawns = 5;
+
+	SpawnerMeshPreview = CreateDefaultSubobject<USkeletalMeshComponent>("HandMesh");
+	SpawnerMeshPreview->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = SpawnerMeshPreview;
+	TeamId = 1;
+}
+
+void ASpawner::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (SpawnedCharacterClass)
+	{
+		auto ClassCDO = SpawnedCharacterClass.GetDefaultObject();
+		if (ClassCDO)
+		{
+			SpawnerMeshPreview->SetSkeletalMesh(ClassCDO->GetMesh()->SkeletalMesh);
+		}
+	}
 }
 
 // Called when the game starts or when spawned
 void ASpawner::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	SpawnPawns();
 }
 
-void ASpawner::SpawnPawns()
+void ASpawner::PostInitProperties()
 {
+	Super::PostInitProperties();
+}
+
+void ASpawner::SpawnPawn()
+{
+	check(SpawnedCharacterClass);
+
+	if (MaxSpawns > 0)
+	{
+		if (NumSpawned >= MaxSpawns)
+		{
+			return;
+		}
+	}
+
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.Owner = this;
 
-	auto SpawnedCharacter = GetWorld()->SpawnActor<AExtendedCharacter>(SpawnedCharacterClass, GetActorTransform(), SpawnParams);
+	auto SpawnedCharacter = GetWorld()->SpawnActorDeferred<AExtendedCharacter>(SpawnedCharacterClass, GetActorTransform(), this);
+	if (SpawnedCharacter)
+	{
+		SpawnedCharacter->TeamId = TeamId;
+		SpawnedCharacter->OnKilledDelegate.AddDynamic(this, &ThisClass::PawnKilled);
+
+		UGameplayStatics::FinishSpawningActor(SpawnedCharacter, GetActorTransform());
+
+		auto AIController = Cast<AAIController>(SpawnedCharacter->GetController());
+		if (AIController && TargetLocation)
+		{
+			AIController->MoveToActor(TargetLocation);
+		}
+
+		SpawnedPawns.Add(SpawnedCharacter);
+		NumSpawned++;
+	}
+}
+
+void ASpawner::PawnKilled(AExtendedCharacter* Character, AController* Killer)
+{
+	NumKilled++;
+
+	OnPawnKilled(Character, Killer);
+
+	if (NumKilled == MaxSpawns)
+	{
+		OnAllPawnsKilled();
+	}
+	else
+	{
+		// Queue up another
+		GetWorldTimerManager().SetTimer(TimerHandle_SpawnPawn, this, &ThisClass::SpawnPawn, RespawnTime);
+	}
 }
--- Source/VRTest/Spawner.h
+++ Source/VRTest/Spawner.h
@@ -7,6 +7,7 @@
 #include "Spawner.generated.h"
 
 class AExtendedCharacter;
+class USkeletalMeshComponent;
 
 UCLASS()
 class VRTEST_API ASpawner : public AActor
@@ -19,11 +20,48 @@ public:
 
 protected:
 	// Called when the game starts or when spawned
+	virtual void OnConstruction(const FTransform& Transform) override;
+
 	virtual void BeginPlay() override;
 
+	virtual void PostInitProperties() override;
+
 	UFUNCTION(BlueprintCallable, Category = "Spawner")
-	void SpawnPawns();
+	void SpawnPawn();
+
+	UFUNCTION()
+	void PawnKilled(AExtendedCharacter* Character, AController* Killer);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnPawnKilled(AExtendedCharacter* Character, AController* Killer);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnAllPawnsKilled();
+
+	UPROPERTY()
+	USkeletalMeshComponent* SpawnerMeshPreview;
 
 	UPROPERTY(EditAnywhere, Category = "Spawner")
 	TSubclassOf<AExtendedCharacter>	SpawnedCharacterClass;
+
+	UPROPERTY(EditAnywhere, Category = "Spawner")
+	int32 MaxSpawns;
+
+	UPROPERTY(EditAnywhere, Category = "Spawner")
+	int32 TeamId;
+
+	UPROPERTY(EditAnywhere, Category = "Spawner")
+	float RespawnTime;
+
+	UPROPERTY(EditInstanceOnly, Category = "Spawner")
+	AActor* TargetLocation;
+
+	UPROPERTY(BlueprintReadOnly)
+	TArray<AExtendedCharacter*>	SpawnedPawns;
+
+	int32 NumSpawned;
+	int32 NumKilled;
+
+	UPROPERTY()
+	FTimerHandle TimerHandle_SpawnPawn;
 };
