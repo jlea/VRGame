3a2b55de7cd462256b43c8c4c7985ceb6d2d84d1
James Lea-Baran <james.c.lea@gmail.com>
2019-03-13 01:06:10 +0100

    Removed most of the HUD code

--- Source/VRTest/UI/VRHUD.cpp
+++ Source/VRTest/UI/VRHUD.cpp
@@ -4,54 +4,11 @@
 #include "Engine/Canvas.h"
 #include "Firearm.h"
 #include "Magazine.h"
+#include "Firearm.h"
 #include "Hand.h"
 #include "PlayerPawn.h"
 
 void AVRHUD::DrawHUD()
 {
 	Super::DrawHUD();
-
-	auto PlayerPawn = Cast<APlayerPawn>(GetOwningPawn());
-	if(!PlayerPawn)
-	{
-		return;
-	}
-
-	TArray<AHand*>	Hands;
-	Hands.Add(PlayerPawn->LeftHand);
-	Hands.Add(PlayerPawn->RightHand);
-
-	for (auto Hand : Hands)
-	{
-		auto HeldFirearm = Cast<AFirearm>(Hand->GetInteractingActor());
-		if (HeldFirearm)
-		{
-			auto HeldMagazine = HeldFirearm->GetLoadedMagazine();
-			if (HeldMagazine)
-			{
-				FString MagazineAmmoText = FString::Printf(TEXT("%d / %d"), HeldMagazine->CurrentAmmo, HeldMagazine->AmmoCount);
-				FVector MagLocation = Canvas->Project(HeldMagazine->GetActorLocation());
-
-				//DrawText(MagazineAmmoText, FColor::White, MagLocation.X, MagLocation.Y, nullptr, 4.0f);
-			}
-		}
-
-		auto HeldMagazine = Cast<AMagazine>(Hand->GetInteractingActor());
-		if (HeldMagazine)
-		{
-			FString MagazineAmmoText = FString::Printf(TEXT("%d / %d"), HeldMagazine->CurrentAmmo, HeldMagazine->AmmoCount);
-			FVector MagLocation = Canvas->Project(HeldMagazine->GetActorLocation());
-
-			//DrawText(MagazineAmmoText, FColor::White, MagLocation.X, MagLocation.Y, nullptr, 4.0f);
-		}
-
-		for (auto NearbyActor : Hand->GetNearbyActors())
-		{
-			if (!Hand->GetInteractingActor())
-			{
-
-				Draw3DLine(NearbyActor->GetActorLocation(), Hand->GetHandSelectionOrigin(), FColor::Green);
-			}
-		}
-	}
 }
